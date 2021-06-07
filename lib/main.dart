29840e398bbc1cec280ff4066d49e5a87baceb79
shashikiran918 <85386298+shashikiran918@users.noreply.github.com>
2021-06-07 14:24:25 +0530

    third

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // to hide debug banner
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('SHASHI'),
+        ),
+        body: Container(
+          color: Colors.red,
+
+          /// Giving dimensions to parent Container
+          /// using MediaQuery
+          /// [container's height] = [(phone's height) / 2]
+          /// [container's width] = [phone's width]
+          height: MediaQuery.of(context).size.height * 0.5,
+          width: MediaQuery.of(context).size.width,
+
+          /// Aligning contents of this Container
+          /// to center
+          alignment: Alignment.center,
+
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return Container(
+                  color: Colors.green,
+
+                  /// Aligning contents of this Container
+                  /// to center
+                  alignment: Alignment.center,
+
+                  /// Using parent's constraints
+                  /// to calculate child's height and width
+                  height: constraints.maxHeight * 0.5,
+                  width: constraints.maxWidth * 0.5,
+                    child: Text(
+                      'LayoutBuilder Widget',
+                      style: TextStyle(
+                        letterSpacing: 3.0,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
